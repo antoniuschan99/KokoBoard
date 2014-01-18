6fda222fb28a8fae2667ae02f0951a9d3fcfcbe6
Anton Chan <antoniuschan99@gmail.com>
2014-01-18 18:10:12 -0500

    added points collection to mongo. also added the reactive context to retrieve points on startup
    Signed-off-by: Anton Chan <antoniuschan99@gmail.com>

--- teamboard.js
+++ teamboard.js
@@ -1,8 +1,28 @@
 if (Meteor.isClient) {
-        Template.canvas.rendered = function(){
-                var canvas = document.getElementById('sketchpad');
-                var context = canvas.getContext('2d');
-				var coordinates = document.getElementById('coordinates');
+	points = new Meteor.Collection('pointsCollection');
+	
+	Deps.autorun( function () {
+	  Meteor.subscribe('pointsSubscription');
+	});
+	
+	Meteor.startup( function() {
+ 	
+	    Deps.autorun( function() {
+	    	var data = points.find({}).fetch();
+			var canvas = document.getElementById('sketchpad');
+			var ctx = canvas.getContext("2d");
+ 			
+			for (i=0; i<data.length; i++) { 
+				ctx.fillRect(i, i, 1, 1);
+			}
+		    //canvas.draw(data);
+ 	  	});
+	});	
+
+    Template.canvas.rendered = function(){
+		var canvas = document.getElementById('sketchpad');
+        var context = canvas.getContext('2d');
+		var coordinates = document.getElementById('coordinates');
                  
                 // create a drawer which tracks touch movements
                 var drawer = {
@@ -11,23 +31,32 @@ if (Meteor.isClient) {
                       context.beginPath();
                       context.moveTo(coors.x, coors.y);
                       this.isDrawing = true;
-				   coordinates.innerHTML = "Coordinates X: " + coors.x + " Coordinates Y: " + coors.y;
-
+				      coordinates.innerHTML = "Coordinates X: " + coors.x + " Coordinates Y: " + coors.y;
+					  points.insert({ 
+					  	x: (coors.x),
+					    y: (coors.y)
+					  });
                    },
                    touchmove: function(coors){
                       if (this.isDrawing) {
                          context.lineTo(coors.x, coors.y);
                          context.stroke();
-					   coordinates.innerHTML = "Coordinates X: " + coors.x + " Coordinates Y: " + coors.y;
-
+					     coordinates.innerHTML = "Coordinates X: " + coors.x + " Coordinates Y: " + coors.y;
+					     points.insert({ 
+						 	x: (coors.x),
+						    y: (coors.y)
+						 });
                       }
                    },
                    touchend: function(coors){
                       if (this.isDrawing) {
                          this.touchmove(coors);
                          this.isDrawing = false;
-					   coordinates.innerHTML = "Coordinates X: " + coors.x + " Coordinates Y: " + coors.y;
-
+					     coordinates.innerHTML = "Coordinates X: " + coors.x + " Coordinates Y: " + coors.y;
+					     points.insert({ 
+						 	x: (coors.x),
+						    y: (coors.y)
+						 });
                       }
                    }
                 };
@@ -53,12 +82,21 @@ if (Meteor.isClient) {
                   event.preventDefault();
                 },false);
         }
-                
 }
 
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+	
+ 	points = new Meteor.Collection('pointsCollection');
 
-  });
+	Meteor.publish('pointsSubscription', function () {
+	  return points.find();
+	});
+
+	Meteor.methods({
+	  'clear': function () {
+	    points.remove({});
+	  }
+	});
+
 }
